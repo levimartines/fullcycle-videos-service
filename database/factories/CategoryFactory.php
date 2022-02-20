848de9a28e587581c7eceea055d79d4978d0a49a
Levi Martines <levi.mferreira@gmail.com>
2022-02-20 15:48:08 -0300

    Initial commit, creating Category resource

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->colorName,
+            'description' => rand(1, 10) % 2 == 0 ?$this->faker->sentence() : null
+        ];
+    }
+}
